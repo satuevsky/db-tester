0fdf1d239e6c1c382a472c1133ebb925cedd7c4e
satuevsky <satuevsky@gmail.com>
2018-05-31 23:20:48 +0400

    Возможность выбора тестов для просмотра из ListView

---
+++ DBTesterUI/Windows/MainWindowTestsTab.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace DBTesterUI.Windows
+{
+    public partial class MainWindow
+    {
+        private void TestsView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(TestModel == null) return;
+            var index = ((ListView) sender).SelectedIndex;
+            TestModel.SelectedTest = TestModel?.Tests[index];
+        }
+    }
+}
