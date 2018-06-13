8bfd1494022abeb93ed697c0449c33912c2a9ad5
satuevsky <satuevsky@gmail.com>
2018-06-13 21:38:02 +0400

    Рефакторинг DbItemsModels

---
+++ DBTesterUI/Models/Config/DbItemsModel/DbItem.cs
@@ -0,0 +1,28 @@
+﻿using DBTesterLib.Db;
+
+namespace DBTesterUI.Models.Config
+{
+    class DbItem
+    {
+        public IDb Db { get; set; }
+
+        private bool _selected;
+
+        public bool Selected
+        {
+            get => _selected;
+            set
+            {
+                _selected = value;
+                _model.OnPropertyChanged();
+            }
+        }
+
+        private DbItemsModel _model;
+
+        public DbItem(DbItemsModel model)
+        {
+            _model = model;
+        }
+    }
+}
--- DBTesterUI/Models/Config/DbItemsModel/DbItemsModel.cs
+++ DBTesterUI/Models/Config/DbItemsModel/DbItemsModel.cs
@@ -7,30 +7,6 @@ using DBTesterUI.Annotations;
 
 namespace DBTesterUI.Models.Config
 {
-    class DbItem
-    {
-        public IDb Db { get; set; }
-
-        private bool _selected;
-
-        public bool Selected
-        {
-            get => _selected;
-            set
-            {
-                _selected = value;
-                _model.OnPropertyChanged();
-            }
-        }
-
-        private DbItemsModel _model;
-
-        public DbItem(DbItemsModel model)
-        {
-            _model = model;
-        }
-    }
-
     class DbItemsModel : INotifyPropertyChanged
     {
         /// <summary>
